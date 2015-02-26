d621a378d41828dc849dc85d9c12838380bbcea5
David Corbin <daconex@gmail.com>
2015-02-25 23:19:14 -0600

    Moved login html to /html folder

--- class.html.php
+++ class.html.php
@@ -8,7 +8,7 @@ class html {
 	}
 	
 	public function login($options) {
-		$form = '<div class="row"><div class="col-lg-12"><div class="well"><form class="form-horizontal" method="post" action="login"><fieldset><legend>Login or <a href="adduser.php">Create Username</a></legend><div class="form-group"><label for="inputEmail" class="col-lg-2 control-label">Username</label><div class="col-lg-10"> <input type="text" class="form-control" id="inputEmail" placeholder="Username" name="un"></div></div><div class="form-group"><label for="inputPassword" class="col-lg-2 control-label">Password</label><div class="col-lg-10"><input type="password" class="form-control" id="inputPassword" placeholder="Password" name="pw"></div></div><div class="form-group"><div class="col-lg-10 col-lg-offset-2"><button type="submit" class="btn btn-primary">Submit</button></div></div></fieldset></form></div></div></div>';
+		include("html/login.inc");
 		
 		if ($options == 'incorrect') {
 			$this->main($this->alertdanger('Incorrect username and/or password!') . $form);
@@ -34,7 +34,7 @@ class html {
 		$this->main($error . $form);
 	}
 	
-		public function settings($content) {
+	public function settings($content) {
 		$this->main($content);
 	}
 
