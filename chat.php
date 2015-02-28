97c59e14f0e49822e8c93b7a9ad737c036ce5979
David Corbin <daconex@gmail.com>
2015-02-27 21:53:54 -0600

    Removed js

--- chat.php
+++ chat.php
@@ -148,47 +148,7 @@ $info = '
     </div>
 
         <script type="text/javascript">
-        	
-            var send = function(formEl) {
-            	if (document.getElementById("sendbutton").value == "") {
-            		return false;
-            	}
 
-                var url = $(formEl).attr("action");
-
-                var data = $("#sendbutton").serializeArray();
-
-                $.ajax({
-                    url: url,
-                    data: data,
-                    type: "post",
-                    success: function() {
-                        document.getElementById("sendbutton").value = "";
-                    }
-                });
-
-                return false;
-            }
-
-            var get = function() {
-
-                $.ajax({
-                    url: "chat",
-                    data: "Update",
-                    type: "get",
-                    cache: "false",
-                    success: function(data) {
-                        if (data!="S") {
-                            $(".chat").html(data);
-                        }
-                    }
-                });
-
-                return false;
-            }
-
-setInterval(function(){get();},2000);
-window.onload = function(){get();}
         </script>
     
 '; 
