aad90f6cd65d8e1a06c3ede55b91c182896665be
David Corbin <daconex@gmail.com>
2015-03-01 21:27:36 -0600

    Moved image upload to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,23 @@ var get = function() {
     });
     return false;
 }
+
+function upload(file) {
+	if (!file || !file.type.match(/image.*/)) return;
+	document.getElementsByClassName("upload")[0].innerHTML = "Uploading...";
+	var fd = new FormData();
+	fd.append("image", file);
+	fd.append("key", "6528448c258cff474ca9701c5bab6927");
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "http://api.imgur.com/2/upload.json"); 
+	xhr.onload = function() {
+		document.querySelector("#link").value = JSON.parse(xhr.responseText).upload.links.original;
+		document.getElementsByClassName("upload")[0].innerHTML = "Upload";
+		document.querySelector(".avatar").src = JSON.parse(xhr.responseText).upload.links.original;
+	}
+	xhr.send(fd);
+}
+
+function clickcall() {
+	document.querySelector("#image").click();
+}
