e5ca14433cf931f8d073e8bc23362ad6d3188eeb
David Corbin <daconex@gmail.com>
2015-03-01 21:26:08 -0600

    Added ability to delete account

--- settings.php
+++ settings.php
@@ -18,10 +18,18 @@ $user = $database->fetch("SELECT * FROM `logins` WHERE username='" . $_SESSION['
 
 $content = "";
 
+// If new link for user image
 if (!empty($_POST) && $_POST['link']!="") {
-	$database->query("UPDATE `logins` SET `avatar`='" .$_POST['link'] . "' WHERE username='" . $_SESSION['un'] . "'");
+	$database->query("UPDATE `logins` SET `avatar`='" . $database->escape($_POST['link']) . "' WHERE username='" . $_SESSION['un'] . "'");
 	$user = $database->fetch("SELECT * FROM `logins` WHERE username='" . $_SESSION['un'] . "'");
 	$content .= $html->alertsuccess("Updated avatar!");
+}
+
+// If account is to be deleted
+if (key($_GET)=="delete") {
+	$database->query("DELETE FROM `logins` WHERE `username`='" . $database->escape($_SESSION["un"]) . "'");
+	// TODO: Add way to tell user that account was deleted, not just redirect
+	header("Location: login.php");
 }
 
 $avatar = $user[0]['avatar']==""?"http://placehold.it/50/FA6F57/fff&text=ME":$user[0]['avatar'];
