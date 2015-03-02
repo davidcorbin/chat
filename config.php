90377ae42b8ea02b561dedba9b654c97c48cfaee
David Corbin <daconex@gmail.com>
2015-03-02 14:10:58 -0600

    Added timeconvert() function from update

--- config.php
+++ config.php
@@ -17,3 +17,26 @@ if ($debug) {
 	ini_set('display_errors',1);
 	error_reporting(-1);
 }
+
+	// Function for converting chat message time to "minutes ago"
+	function timeconvert($ptime) {
+		$etime = time() - $ptime;
+		if ($etime < 1) {
+			return '0 seconds';
+		}
+		$a = array(12 * 30 * 24 * 60 * 60 => 'year',
+				30 * 24 * 60 * 60 => 'month',
+				24 * 60 * 60 => 'day',
+				60 * 60 => 'hour',
+				60 => 'minute',
+				1 => 'second'
+		);
+		foreach ($a as $secs => $str) {
+			$d = $etime / $secs;
+			if ($d >= 1) {
+				$r = round($d);
+				return $r . ' ' . $str . ($r > 1 ? 's' : '') . ' ago';
+			}
+		}
+	}
+	
