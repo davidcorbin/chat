474ff8265f85f1ff7417417fccdb331a7ad8cd33
David Corbin <daconex@gmail.com>
2015-03-02 14:10:23 -0600

    Removed timeconvert() function and put in config

--- chatupdate.php
+++ chatupdate.php
@@ -23,27 +23,7 @@ if (!isset($check)) {
 
 // Update chat window content elseif 
 if (!empty($_GET) && isset($_GET['Update'])) {
-	// Function for converting chat message time to "minutes ago"
-	function timeconvert($ptime) {
-		$etime = time() - $ptime;
-		if ($etime < 1) {
-			return '0 seconds';
-		}
-		$a = array(12 * 30 * 24 * 60 * 60 => 'year',
-				30 * 24 * 60 * 60 => 'month',
-				24 * 60 * 60 => 'day',
-				60 * 60 => 'hour',
-				60 => 'minute',
-				1 => 'second'
-		);
-		foreach ($a as $secs => $str) {
-			$d = $etime / $secs;
-			if ($d >= 1) {
-				$r = round($d);
-				return $r . ' ' . $str . ($r > 1 ? 's' : '') . ' ago';
-			}
-		}
-	}
+
 	// Fetch database values for latest comments and user data
 	$chat = $database->fetch("SELECT * FROM `chat` ORDER BY date DESC LIMIT 50");
 	$me = $database->fetch("SELECT * FROM `logins` WHERE username='" . $_SESSION['un'] . "'");
