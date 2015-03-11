8c84d6d7a4d20584c0917ac2405f08ce8bec524d
David Corbin <daconex@gmail.com>
2015-03-11 09:24:42 -0500

    Get messages from specific chat

--- chatupdate.php
+++ chatupdate.php
@@ -21,72 +21,65 @@ if (!isset($check)) {
 	die();
 }
 
-// Update chat window content elseif 
-if (!empty($_GET) && isset($_GET['Update'])) {
+$chat = key($_GET);
 
-	// Fetch database values for latest comments and user data
-	$chat = $database->fetch("SELECT * FROM `chat` ORDER BY date DESC LIMIT 50");
-	$me = $database->fetch("SELECT * FROM `logins` WHERE username='" . $_SESSION['un'] . "'");
+// Exit if table doesn't exist
+if (!$database->tableexists($chat)) {
+	echo "No";
+	exit();
+}
+
+
+// Fetch database values for latest comments and user data
+$chat = $database->fetch("SELECT * FROM " . $chat ." ORDER BY date DESC LIMIT 50");
+
+$me = $database->fetch("SELECT * FROM `logins` WHERE username='" . $_SESSION['un'] . "'");
+
+
+// Create html 
+for ($i = 0; $i < count($chat); $i++) {
+	// If post is my me
+	if ($chat[$i]['user'] == $_SESSION['un']) {
 	
-	/*
-	// If no new posts
-	if (isset($_SESSION['latest_update']) && $_SESSION['latest_update']==$chat[0]["id"]) 
-{
-		echo "S";
-		exit();
-	}
-	*/
-	
-	// Set session var for latest update from client
-	$_SESSION['latest_update'] = $chat[0]["id"];
-	
-	for ($i = 0; $i < count($chat); $i++) {
-		// If post is my me
-		if ($chat[$i]['user'] == $_SESSION['un']) {
+		if ($me[0]['avatar']=="") {
+			$avatar = "http://placehold.it/50/FA6F57/fff&text=ME";
+		}
+		else {
+			$avatar = $me[0]['avatar'];
+		}
+		echo '<li class="right clearfix"><span class="chat-img pull-right"><a target="_blank" href="profile/' . $chat[$i]['user'] . '"><img src="' . $avatar . '" alt="User Avatar" class="img-circle avatar" /></a></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">';
 		
-			if ($me[0]['avatar']=="") {
-				$avatar = "http://placehold.it/50/FA6F57/fff&text=ME";
-			}
-			else {
-				$avatar = $me[0]['avatar'];
-			}
-			echo '<li class="right clearfix"><span class="chat-img pull-right"><a target="_blank" href="profile?' . $chat[$i]['user'] . '"><img src="' . $avatar . '" alt="User Avatar" class="img-circle avatar" /></a></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">';
-			
-			// If admin, show icon in front of name
-			echo $me[0]['type']=="admin"?'<span class="glyphicon glyphicon-flash" style="color:black;"></span>':'';
-			echo $chat[$i]['user'];
-			echo '</strong><small class="pull-right text-muted"> <span class="glyphicon glyphicon-time"></span>';
-			echo timeconvert($chat[$i]['date']);
-			echo '</small></div><p>';
-			echo htmlspecialchars($chat[$i]['data']);
-			echo '</p></div></li>';
+		// If admin, show icon in front of name
+		echo $me[0]['type']=="admin"?'<span class="glyphicon glyphicon-flash" style="color:black;"></span>':'';
+		echo $chat[$i]['user'];
+		echo '</strong><small class="pull-right text-muted"> <span class="glyphicon glyphicon-time"></span>';
+		echo timeconvert($chat[$i]['date']);
+		echo '</small></div><p>';
+		echo htmlspecialchars($chat[$i]['data']);
+		echo '</p></div></li>';
+	}
+	
+	// If post is by anyone else
+	else {
+	
+		$chat_user = $database->fetch("SELECT * FROM `logins` WHERE username='" . $chat[$i]['user'] . "'");
+	
+		if (empty($chat_user) || $chat_user[0]['avatar']=="") {
+			$avatar = "http://placehold.it/50/55C1E7/fff&text=U";
+		}
+		else {
+			$avatar = $chat_user[0]['avatar'];
 		}
 		
-		// If post is by anyone else
-		else {
+		echo '<li class="left clearfix"><span class="chat-img pull-left"><a target="_blank" href="profile/' . $chat[$i]['user'] . '"><img src="' . $avatar . '" alt="User Avatar" class="img-circle avatar" /></a></span><div class="chat-body clearfix"><div class="header"><small class="text-muted"><span class="glyphicon glyphicon-time"></span>';
+		echo timeconvert($chat[$i]['date']);
+		echo '</small><strong class="pull-right primary-font">';
 		
-			$chat_user = $database->fetch("SELECT * FROM `logins` WHERE username='" . $chat[$i]['user'] . "'");
-		
-			if (empty($chat_user) || $chat_user[0]['avatar']=="") {
-				$avatar = "http://placehold.it/50/55C1E7/fff&text=U";
-			}
-			else {
-				$avatar = $chat_user[0]['avatar'];
-			}
-			
-			echo '<li class="left clearfix"><span class="chat-img pull-left"><a target="_blank" href="profile?' . $chat[$i]['user'] . '"><img src="' . $avatar . '" alt="User Avatar" class="img-circle avatar" /></a></span><div class="chat-body clearfix"><div class="header"><small class="text-muted"><span class="glyphicon glyphicon-time"></span>';
-			echo timeconvert($chat[$i]['date']);
-			echo '</small><strong class="pull-right primary-font">';
-			
-			// If admin show icon in front of name
-			echo !empty($chat_user) && $chat_user[0]['type']=="admin"?'<span class="glyphicon glyphicon-flash" style="color:black;"></span>':'';
-			echo $chat[$i]['user'];
-			echo '</strong></div><p>';
-			echo htmlspecialchars($chat[$i]['data']);
-			echo '</p></div></li>';
-		}		
-	}
-}
-else {
-	echo "Error";
+		// If admin show icon in front of name
+		echo !empty($chat_user) && $chat_user[0]['type']=="admin"?'<span class="glyphicon glyphicon-flash" style="color:black;"></span>':'';
+		echo $chat[$i]['user'];
+		echo '</strong></div><p>';
+		echo htmlspecialchars($chat[$i]['data']);
+		echo '</p></div></li>';
+	}		
 }
