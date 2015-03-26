00df967b098a1d3627dee85d9c9c9ec814c38797
David Corbin <daconex@gmail.com>
2015-03-26 11:36:56 -0500

    Commented out session check

--- profile.php
+++ profile.php
@@ -5,6 +5,7 @@ require("config.php");
 require_once('class.db.php');
 $database = new db;
 
+/*
 if (isset($_SESSION['un'])) {
 	$theme = $database->fetch("SELECT theme FROM `logins` WHERE username = '" . $_SESSION['un'] . "'");
 	$theme = $theme[0]['theme'];
@@ -17,67 +18,39 @@ else {
 	require_once('class.html.php');
 	$html = new html;
 }
+*/
 
 // Get first GET key and escape
-$user = $database->escape(key($_GET));
+$username = $database->escape(key($_GET));
 
-$userdata = $database->fetch("SELECT * FROM `logins` WHERE username = '" . $user . "'");
-
-// Check if user exists
-if (empty($userdata)) {
-	$html->profile("Username doesn't exist. The account must have been deleted or removed.");
-}
+require_once("class.user.php");
+$user = new user($username);
 
 // Add profile view
-if (!isset($_SESSION['un']) || $_SESSION['un']!=$userdata[0]['username']) {
-	$database->query("UPDATE logins SET `profile_view_count` = profile_view_count + 1 WHERE username = '" . $userdata[0]['username'] . "'");
-	// Reupdate user database data
-	$userdata = $database->fetch("SELECT * FROM `logins` WHERE username = '" . $user . "'");
+if (!isset($_SESSION['un']) || $_SESSION['un']!=$username) {
+    $database->query("UPDATE logins SET `profile_view_count` = profile_view_count + 1 WHERE username = '" . $username . "'");
 }
 
 // If logged in and looking at my profile
-if (isset($_SESSION['un']) && $_SESSION['un']==$userdata[0]['username']) {
-	$avatar = $userdata[0]['avatar']!=""?$userdata[0]['avatar']:"http://placehold.it/50/FA6F57/fff&text=ME";
+if (isset($_SESSION['un']) && $_SESSION['un']==$username) {
+    $avatar = $user->getAvatar()!=""?$user->getAvatar():"http://placehold.it/50/FA6F57/fff&text=ME";
 }
 else {
-	$avatar = $userdata[0]['avatar']!=""?$userdata[0]['avatar']:"http://placehold.it/50/55C1E7/fff&text=U";
+    $avatar = $user->getAvatar()!=""?$user->getAvatar():"http://placehold.it/50/55C1E7/fff&text=U";
 }
 
-$user = '
+$values = array();
+$values['un'] = $user->getUsername();
+$values['avat'] = $avatar;
+$values['team'] = $user->getTeam();
+$values['pos'] = $user->getPosition();
+$values['loc'] = $user->getLocation();
+$values['site'] = $user->getWebsite();
+$values['createat'] = timeconvert($user->getCreatedAt());
+$values['views'] = $user->getProfileViews();
+$values['posts'] = $user->getPostCount();
 
-<div class="col-md-8 col-lg-6 col-centered">
-    <div class="list-group">
-        <a class="list-group-item active">' . $userdata[0]['username'] . '</a>
-        <div class="list-group-item">
-            <img src="' . $avatar . '" class="img-circle" style="width:100px; height:100px; margin: auto; display:block;">';
+$content = json_encode($values);
 
-$user .= $userdata[0]['team_num']!="0"?'
-        <br>
-        Team: ' . $userdata[0]['team_num']:"";
+echo $content;
 
-$user .= $userdata[0]['position']!=""?'
-        <br>
-        Position: ' . $userdata[0]['position']:"";
-
-$user .= $userdata[0]['location']!=""?'
-        <br>
-        Location: ' . $userdata[0]['location']:"";
-
-$user .= $userdata[0]['website']!=""?'
-        <br>
-        Site: <a target="_blank" href="' . $userdata[0]['website'] . '">' . $userdata[0]['website'] . '</a>':"";
-
-$user .= '
-        <br>
-        Created: ' . timeconvert($userdata[0]['created_at']) . '
-        <br>
-        Profile views: ' . $userdata[0]['profile_view_count'] . '
-        <br>
-        Number of posts: ' . $userdata[0]['post_count'] . '
-        </div>
-    </div>
-</div>
-
-';
-
-$html->profile($user);
