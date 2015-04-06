fc0b1c5b167dda1fe6b9fea6534c79bb4a5af2b6
David Corbin <daconex@gmail.com>
2015-04-06 17:14:32 -0500

    Increment number of logins in database

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 
 require("config.php");
 
+// Logout by deleting session username and password
 unset($_SESSION['un']);
 unset($_SESSION['pw']);
 
@@ -13,14 +14,20 @@ $html = new html;
 
 if (isset($_POST["un"]) && isset($_POST["pw"])) {
 	$check = $database->auth($_POST["un"], $_POST["pw"]);
+
+    // Username and password correct
 	if ($check) {
 		// Set session vars
 		$_SESSION['un'] = $_POST["un"];
 		$_SESSION['pw'] = $_POST["pw"];
-		
+
+        $database->query("UPDATE logins SET `logins` = logins + 1 WHERE username = '" . $_SESSION['un'] . "'");
+
 		// Redirect 
 		header("Location: chat.php");
 	}
+
+    // Username and password incorrect
 	else {		
 		$html->login("incorrect");
 	}
