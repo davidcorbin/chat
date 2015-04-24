70a5a62f69eb8dc7633329d29268ebf38b4b6990
David Corbin <daconex@gmail.com>
2015-04-24 17:56:28 -0500

    Removed old commented code

--- chatupdate.php
+++ chatupdate.php
@@ -41,8 +41,12 @@ catch (Exception $e) {
     }
 }
 
+// Convert get value from string to int
+$_GET['latestpost'] = intval($_GET['latestpost']);
+
 $chat->getPostsAfterId($_GET['latestpost'], 50);
 $resultjson = $chat->getJson();
+
 $decoded = json_decode($resultjson, true);
 
 // Parse each post in array
@@ -86,68 +90,3 @@ for ($i = 0; $i < count($decoded); $i++) {
 
 print_r(json_encode($decoded));
 exit();
-
-
-
-
-
-
-
-// Fetch database values for latest comments and user data
-$chat = $database->fetch("SELECT * FROM " . $chat ." ORDER BY date DESC LIMIT 50");
-
-$me = $database->fetch("SELECT * FROM `logins` WHERE username='" . $_SESSION['un'] . "'");
-
-// If no posts in chat
-if (count($chat)==0) {
-	echo "No posts yet. Be the first!";
-	exit();
-}
-
-// Create html 
-for ($i = 0; $i < count($chat); $i++) {
-	// If post is my me
-	if ($chat[$i]['user'] == $_SESSION['un']) {
-	
-		if ($me[0]['avatar']=="") {
-			$avatar = "http://placehold.it/50/FA6F57/fff&text=ME";
-		}
-		else {
-			$avatar = $me[0]['avatar'];
-		}
-		echo '<li class="right clearfix"><span class="chat-img pull-right"><a data-toggle="modal" data-target="#profileView" data-user="' . $chat[$i]['user'] . '"><img src="' . $avatar . '" alt="User Avatar" class="img-circle avatar" /></a></span><div class="chat-body clearfix"><div class="header"><strong class="primary-font">';
-		
-		// If admin, show icon in front of name
-		echo $me[0]['type']=="admin"?'<span class="glyphicon glyphicon-flash" style="color:black;"></span>':'';
-		echo $chat[$i]['user'];
-		echo '</strong><small class="pull-right text-muted"> <span class="glyphicon glyphicon-time"></span>';
-		echo timeconvert($chat[$i]['date']);
-		echo '</small></div><p>';
-		echo htmlspecialchars($chat[$i]['data']);
-		echo '</p></div></li>';
-	}
-	
-	// If post is by anyone else
-	else {
-	
-		$chat_user = $database->fetch("SELECT * FROM `logins` WHERE username='" . $chat[$i]['user'] . "'");
-	
-		if (empty($chat_user) || $chat_user[0]['avatar']=="") {
-			$avatar = "http://placehold.it/50/55C1E7/fff&text=U";
-		}
-		else {
-			$avatar = $chat_user[0]['avatar'];
-		}
-		
-		echo '<li class="left clearfix"><span class="chat-img pull-left"><a data-toggle="modal" data-target="#profileView" data-user="' . $chat[$i]['user'] . '"><img src="' . $avatar . '" alt="User Avatar" class="img-circle avatar" /></a></span><div class="chat-body clearfix"><div class="header"><small class="text-muted"><span class="glyphicon glyphicon-time"></span>';
-		echo timeconvert($chat[$i]['date']);
-		echo '</small><strong class="pull-right primary-font">';
-		
-		// If admin show icon in front of name
-		echo !empty($chat_user) && $chat_user[0]['type']=="admin"?'<span class="glyphicon glyphicon-flash" style="color:black;"></span>':'';
-		echo $chat[$i]['user'];
-		echo '</strong></div><p>';
-		echo htmlspecialchars($chat[$i]['data']);
-		echo '</p></div></li>';
-	}		
-}
